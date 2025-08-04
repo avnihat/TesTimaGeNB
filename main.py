c7bc4e1c9b47c135d092baee23efbc3a8a5ebe54
avnihat <av.nihatbas@gmail.com>
2025-08-04 19:17:29 +0300

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-import sys
-from PyQt5.QtWidgets import QApplication
-from gui.interface import TesTimaGeNBWindow
-
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    window = TesTimaGeNBWindow()
-    window.show()
-    sys.exit(app.exec_())
