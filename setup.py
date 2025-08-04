b5b7f2e8cdc294ea5acc661056b098dc04374d33
avnihat <av.nihatbas@gmail.com>
2025-08-04 21:33:40 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,16 @@
-from setuptools import setup, find_packages
+from setuptools import setup
+
+APP = ['main.py']
+DATA_FILES = []
+OPTIONS = {
+    'argv_emulation': True,
+    'packages': ['PyQt5', 'cv2', 'tensorflow', 'PIL', 'numpy', 'imagehash', 'exifread'],
+}
 
 setup(
+    app=APP,
     name='TesTimaGeNB',
-    version='1.0.0',
-    description='Görsellerin gerçeklik analizini yapan adli yazılım',
-    author='Senin Adın',
-    packages=find_packages(),
-    install_requires=[
-        'PyQt5',
-        'opencv-python',
-        'Pillow',
-        'imagehash',
-        'qrcode',
-        'reportlab',
-        'tensorflow',
-        'keras',
-        'exifread'
-    ],
-    entry_points={
-        'console_scripts': [
-            'testimagenb = main:main'
-        ]
-    },
-    include_package_data=True,
+    data_files=DATA_FILES,
+    options={'py2app': OPTIONS},
+    setup_requires=['py2app'],
 )
