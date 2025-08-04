91ec88a4be0de0167bf391f7e08fa2e699bbb8f5
avnihat <av.nihatbas@gmail.com>
2025-08-04 22:29:43 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,33 @@
 from setuptools import setup
 
-APP = ['main.py']
+APP = ['main.py']  # Ana dosya
 DATA_FILES = []
+
 OPTIONS = {
-    'argv_emulation': True,
-    'packages': ['PyQt5', 'cv2', 'tensorflow', 'PIL', 'numpy', 'imagehash', 'exifread'],
+    'argv_emulation': True,  # MacOS argümanlarını terminalsiz çalıştırmak için
+    'packages': [
+        'PyQt5',
+        'cv2',
+        'tensorflow',
+        'PIL',
+        'numpy',
+        'imagehash',
+        'exifread'
+    ],
+    'includes': [
+        'os',
+        'sys',
+        'threading',
+        'importlib',
+    ],
+    'iconfile': 'assets/logo.icns',  # (isteğe bağlı) .app için ikon
+    'plist': {
+        'CFBundleName': 'TesTimaGeNB',
+        'CFBundleDisplayName': 'TesTimaGeNB',
+        'CFBundleIdentifier': 'com.yourdomain.testimagenb',
+        'CFBundleVersion': '1.0',
+        'CFBundleShortVersionString': '1.0',
+    }
 }
 
 setup(
